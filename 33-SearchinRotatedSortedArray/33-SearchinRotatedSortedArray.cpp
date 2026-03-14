eb8e91d3c7496a98a606a7af1daa4a675fabfdf2
Gadaputi Balaji <153218171+balaji-gadaputi@users.noreply.github.com>
2026-03-14 14:06:34 +0530

    File updated at 14/03/2026, 14:06:38

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.cpp
@@ -1,30 +1,20 @@
-// Last updated: 14/03/2026, 13:40:17
+// Last updated: 14/03/2026, 14:06:38
 1class Solution {
 2public:
 3    int search(vector<int>& nums, int target) {
-4        int pivot=(0+((nums.size()-1)-0)/2);
-5        if(nums.size() == 1 && nums[0] != target) return -1;
-6        else if(nums.size() == 1 && nums[0] == target) return 0;
-7
-8        for(int i=0; i<nums.size()-1; i++){
-9            if(nums[i] > nums[i+1]){
-10                pivot = i+1;
+4        int low = 0, high = nums.size()-1;
+5        while(low<=high){
+6            int mid = low + (high-low)/2;
+7            if(nums[mid] == target) return mid;
+8            else if(nums[low]<=nums[mid]){ // left half is sorted
+9                if(target<nums[mid] && target>=nums[low]) high = mid-1;
+10                else low = mid+1;
 11            }
-12        }
-13        int low = 0, high = pivot-1;
-14        while(low<=high){
-15            int mid = low + (high-low)/2;
-16            if(nums[mid] == target) return mid;
-17            else if(nums[mid] > target) high = mid-1;
-18            else low = mid+1;
-19        }
-20        low = pivot; high = nums.size()-1;
-21        while(low<=high){
-22            int mid = low + (high-low)/2;
-23            if(nums[mid] == target) return mid;
-24            else if(nums[mid] > target) high = mid-1;
-25            else low = mid+1;
-26        }
-27        return -1;
-28    }
-29};
+12            else{ // right half is sorted
+13                if(target>nums[mid] && target<=nums[high]) low = mid+1;
+14                else high = mid-1;
+15            }
+16        }
+17        return -1;
+18    }
+19};
