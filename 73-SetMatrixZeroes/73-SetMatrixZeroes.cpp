4c46385b335d78fd346dc1d5dc8ed0bcfaac200b
Gadaputi Balaji <153218171+balaji-gadaputi@users.noreply.github.com>
2026-07-06 12:00:30 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -0,0 +1,22 @@
+// Last updated: 06/07/2026, 12:00:31
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int> row,col;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j]==0){row.push_back(i);col.push_back(j);}
+            }
+        }
+        for(int i=0;i<row.size();i++){
+            for(int j=0;j<matrix[row[i]].size();j++){
+                matrix[row[i]][j]=0;
+            }
+        }
+        for(int i=0;i<col.size();i++){
+            for(int j=0;j<matrix.size();j++){
+                matrix[j][col[i]]=0;
+            }
+        }
+    }
+};
