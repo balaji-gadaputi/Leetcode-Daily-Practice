d161a51b6d09ca59528ca49d1e512a9d4744f697
Gadaputi Balaji <153218171+balaji-gadaputi@users.noreply.github.com>
2026-07-06 12:00:42 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.cpp
@@ -0,0 +1,13 @@
+// Last updated: 06/07/2026, 12:00:43
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int farthest = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i > farthest) return false;
+            farthest = max(farthest, i+nums[i]);
+            if(farthest >= nums.size()-1 ) return true;
+        }
+        return true;
+    }
+};
