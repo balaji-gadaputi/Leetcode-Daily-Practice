e326766b10ce6b9a8d3d38742cdbd884946c05ab
Gadaputi Balaji <153218171+balaji-gadaputi@users.noreply.github.com>
2026-07-06 12:00:05 +0530

    Create 134-GasStation

---
+++ 134-GasStation/134-GasStation.cpp
@@ -0,0 +1,20 @@
+// Last updated: 06/07/2026, 12:00:06
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalGas=0,totalCost=0;
+        for(int i=0;i<gas.size();i++){
+            totalGas += gas[i]; totalCost += cost[i];
+        }
+        if(totalGas < totalCost) return -1;
+        int start=0, tank=0;
+        for (int i = 0; i < gas.size(); i++) {
+            tank += gas[i] - cost[i];
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return start;
+    }
+};
